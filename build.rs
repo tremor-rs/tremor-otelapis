426427fcff80b820c6b04be6a3b8935df3faa05c
Heinz N. Gies <heinz@licenser.net>
2021-08-11 14:24:53 +0200

    Don't format
    Signed-off-by: Heinz N. Gies <heinz@licenser.net>

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ fn main() {
     tonic_build::configure()
     .build_client(true)
     .build_server(true)
-    .format(true)
+    .format(false)
     .compile(&[
         "opentelemetry-proto/opentelemetry/proto/collector/logs/v1/logs_service.proto",
         "opentelemetry-proto/opentelemetry/proto/collector/metrics/v1/metrics_service.proto",
